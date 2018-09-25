829817dc0bc7b8fb20b5fe0a6e3db80a3fc95fa8
The Bao <thebao.dev@gmail.com>
2018-09-25 11:56:30 +0700

    Added Time Component

--- src/components/FileListItem.js
+++ src/components/FileListItem.js
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FileName from './FileName';
 import CommitMessage from './CommitMessage';
+import Time from './Time';
 
 const FileListItem = ({ file }) => (
   <tr className="file-list-item">
     <FileName name={file.name} type={file.type} />
     <CommitMessage commit={file.latestCommit} />
+    <td className="age">
+      <Time time={file.updated_at} />
+    </td>
   </tr>
 );
 FileListItem.propTypes = {
